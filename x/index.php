9b526e7593eb801afafc29ec5fa9f66afa68a6dc
tom <tom@dev-server>
2015-05-28 13:30:08 -0500

    Changes to the code

--- x/index.php
+++ x/index.php
@@ -1,8 +1,8 @@
 <?php
-//session_start();
-//include 'baseline/constants.php';
-//$header = 'Location: ' . \X\Blc\REDIRECTSECURE . 'x.php';
-//header($header);
-//exit();
-print 'Hello worldzy';
+session_start();
+include 'baseline/constants.php';
+$header = 'Location: ' . \X\Blc\REDIRECTSECURE . 'x.php';
+header($header);
+exit();
+//print 'Hello worldzy';
 
